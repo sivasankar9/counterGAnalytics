35bd61367313ed2a7fa97b70a930e1c8166ba873
sivasankar <sivasankar.g@perfios.com>
2022-12-03 13:22:01 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './style.css'
+
+class App extends React.Component {
+  state = {
+    counter: 0
+  }
+
+  increaseCounter = () => {
+    this.setState({ counter: this.state.counter + 1 })
+  }
+
+  decreaseCounter = () => {
+    this.setState({ counter: this.state.counter - 1 })
+  }
+
+  render() {
+    return <div>
+      <h1>{this.state.counter}</h1>
+      <button onClick={this.decreaseCounter}>
+        -
+      </button>
+      <button onClick={this.increaseCounter}>
+        +
+      </button>
+    </div>
+  }
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'))
